b6677f7d82db358bddea2e864a8b856a4875585e
Mark Oral Hernandez <blackcoder41@gmail.com>
2019-11-15 22:29:12 +0800

    first commit

---
+++ src/main/java/com/raketlabs/redirect/RaketLabsApplication.java
@@ -0,0 +1,13 @@
+package com.raketlabs.redirect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaketLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaketLabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raketlabs/redirect/RaketLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raketlabs.redirect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaketLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
